a7e80a9e3f358b9c4e76b70bd5e79745c858061a
Rajan Tripathi <tripathirajan3@gmail.com>
2023-02-26 22:04:55 +0530

    1. background color change 2. bug fixes

--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -13,6 +13,7 @@ import * as Yup from 'yup';
 import { useDispatch } from 'react-redux';
 import { login } from '../../storage/actions/authAction';
 import AppAlert from '../../components/Alert';
+import { PageWithTitle } from '../../components/Page';
 
 const LoginSchema = Yup.object().shape({
     username: Yup.string().required('Email is required'),
@@ -51,82 +52,85 @@ const Login = () => {
     const { errors, touched, values, isSubmitting, handleSubmit, getFieldProps } = formik;
 
     return (
-        <Stack
-            direction="column"
-            spacing={3}
+        <PageWithTitle
+            title="Login | Merno"
         >
-            <Box>
-                <Typography variant='h4' color="secondary">
-                    Welcome to MERNO
-                </Typography>
-                <Typography variant='caption' sx={{ color: 'text.secondary', mt: 1 }}>
-                    Sign in to your account
-                </Typography>
-            </Box>
-            {
-                errMsg && <AppAlert body={errMsg} />
-            }
-            <FormikProvider value={formik}>
-
-                <Box
-                    component={Form}
-                    onSubmit={handleSubmit}
-                    autoComplete="off"
-                    noValidate
-                    sx={
-                        {
-                            width: '100%'
-                        }
-                    }
-                >
-                    <Stack spacing={3}>
-                        <TextField
-                            fullWidth
-                            placeholder='Enter username or email'
-                            autoComplete="username"
-                            type="username"
-                            label="Username"
-                            size="small"
-                            {...getFieldProps('username')}
-                            error={Boolean(touched.username && errors.username)}
-                            helperText={touched.username && errors.username}
-                        />
-                        <TextField
-                            fullWidth
-                            placeholder='Enter your password'
-                            autoComplete="current-password"
-                            type={'password'}
-                            label="Password"
-                            size='small'
-                            {...getFieldProps('password')}
-                            error={Boolean(touched.password && errors.password)}
-                            helperText={touched.password && errors.password}
-                        />
-                    </Stack>
-                    <Stack direction="row" alignItems="center" justifyContent="space-between" sx={{ my: 2 }}>
-                        <FormControlLabel
-                            control={<Checkbox {...getFieldProps('remember')} checked={values.remember} />}
-                            label="Remember me"
-                        />
-
-                        <Link component={RouterLink} variant="subtitle2" to="/forgotPassword">
-                            Forgot password?
-                        </Link>
-                    </Stack>
-                    <Button
-                        fullWidth
-                        size="large"
-                        type="submit"
-                        variant="contained"
-                        color="secondary"
-                        disabled={isSubmitting}
-                    >
-                        Login
-                    </Button>
+            <Stack
+                direction="column"
+                spacing={3}
+            >
+                <Box>
+                    <Typography variant='h4' color="secondary">
+                        Welcome to MERNO
+                    </Typography>
+                    <Typography variant='caption' sx={{ color: 'text.secondary', mt: 1 }}>
+                        Sign in to your account
+                    </Typography>
                 </Box>
-            </FormikProvider>
-        </Stack>
+                {
+                    errMsg && <AppAlert body={errMsg} />
+                }
+                <FormikProvider value={formik}>
 
+                    <Box
+                        component={Form}
+                        onSubmit={handleSubmit}
+                        autoComplete="off"
+                        noValidate
+                        sx={
+                            {
+                                width: '100%'
+                            }
+                        }
+                    >
+                        <Stack spacing={3}>
+                            <TextField
+                                fullWidth
+                                placeholder='Enter username or email'
+                                autoComplete="username"
+                                type="username"
+                                label="Username"
+                                size="small"
+                                {...getFieldProps('username')}
+                                error={Boolean(touched.username && errors.username)}
+                                helperText={touched.username && errors.username}
+                            />
+                            <TextField
+                                fullWidth
+                                placeholder='Enter your password'
+                                autoComplete="current-password"
+                                type={'password'}
+                                label="Password"
+                                size='small'
+                                {...getFieldProps('password')}
+                                error={Boolean(touched.password && errors.password)}
+                                helperText={touched.password && errors.password}
+                            />
+                        </Stack>
+                        <Stack direction="row" alignItems="center" justifyContent="space-between" sx={{ my: 2 }}>
+                            <FormControlLabel
+                                control={<Checkbox {...getFieldProps('remember')} checked={values.remember} />}
+                                label="Remember me"
+                            />
+
+                            <Link component={RouterLink} variant="subtitle2" to="/forgotPassword">
+                                Forgot password?
+                            </Link>
+                        </Stack>
+                        <Button
+                            fullWidth
+                            size="normal"
+                            type="submit"
+                            variant="contained"
+                            color="secondary"
+                            disabled={isSubmitting}
+                        >
+                            Login
+                        </Button>
+                    </Box>
+                </FormikProvider>
+            </Stack>
+        </PageWithTitle>
     )
 }
 
